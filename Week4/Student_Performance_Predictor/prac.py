ee207c4d26e57ba161991110a180734f6f90251a
Shubham kusale <98016063+shubhamkusale@users.noreply.github.com>
2026-03-22 18:25:54 +0530

    Week 4: Linear Regression + Gradient Descent from scratch

--- Week4/Student_Performance_Predictor/prac.py
+++ Week4/Student_Performance_Predictor/prac.py
@@ -1,29 +1,32 @@
 import numpy as np
 
-X = np.array([1, 2, 3, 4, 5])   
-y = np.array([20, 40, 50, 65, 80]) 
 
-weight = 0.0
-bias = 0.0
-learning_rate = 0.01
-epoches = 1000
+class LinearRegression:
+    def __init__(self, lr = 0.01, epoches = 1000):
+        self.weight = 0.0
+        self.bias = 0.0
+        self.learning_rate = lr
+        self.epoch = epoches
 
-for epoch in range(epoches):
-    y_pred = weight * X + bias
-    
-    loss = np.mean((y_pred - y)**2)
+    def fit(self, X, y):
+        for epoces in range(self.epoch):
+            y_pred = self.weight * X + self.bias
+            loss = np.mean((y_pred - y)** 2)
+            dw = np.mean(2*(y_pred - y)* X)
+            db = np.mean(2*(y_pred - y))
+            self.weight -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+            if epoces %100 == 0:
+                print(f"epoches :{epoces}, loss {loss:.2f}")
+            
+    def predict(self, X):
+        return self.weight * X + self.bias
 
-    dw = np.mean(2 *(y_pred - y) *X)
-    db = np.mean(2 *(y_pred - y))
+X = np.array([1, 2, 3, 4, 5])
+y = np.array([20, 40, 50, 65, 80])
 
-    weight = weight - learning_rate * dw
-    bias = bias - learning_rate * db
+model = LinearRegression()
+model.fit(X, y)
 
-    if epoch % 100 == 0:
-        print(f"epoches {epoch:4d}| Weight {weight:.2f}| Bias {bias:.2f}")
-
-print("Traning Complete ")
-print(f"Weight: {weight:.2f}")
-print(f"Bias: {bias:.2f}")    
-print(f"Study 6 hours → {weight * 6 + bias:.1f} marks")
-print(f"Study 8 hours → {weight * 8 + bias:.1f} marks")
+print(f"6 hours → {model.predict(6):.1f} marks")
+print(f"8 hours → {model.predict(8):.1f} marks")
