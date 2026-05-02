5f81cc5701631b1e060c03e251a953cf821de205
Shubham kusale <98016063+shubhamkusale@users.noreply.github.com>
2026-05-02 18:20:24 +0530

    Week7: Spam classifier complete - 99.2% accuracy with Naive Bayes

--- WEEK7/spam_classifier.py
+++ WEEK7/spam_classifier.py
@@ -11,3 +11,35 @@ df = pd.read_csv('https://raw.githubusercontent.com/justmarkham/pycon-2016-tutor
 print(df.head())
 print(df.shape)
 print(df['label'].value_counts())
+
+df['label'] = df['label'].map({'ham':0, 'spam': 1})
+
+X = df['message']
+y = df['label']
+
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size= 0.2 , random_state= 42)
+
+vectorizer = CountVectorizer()
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
+
+print(X_train_vec.shape)
+print(f"training Samples :{X_train.shape[0]}")
+
+model = MultinomialNB()
+model.fit(X_train_vec, y_train)
+
+y_pred = model.predict(X_test_vec)
+
+print("Accuracy :", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
+def predict_email(email):
+    email_vec = vectorizer.transform([email])
+    prediction = model.predict(email_vec)
+    return "SPAM" if prediction[0] == 1 else "HAM"
+
+
+print(predict_email("Congratulations! You won a free iPhone click now"))
+print(predict_email("Hey are we meeting tomorrow at 3pm?"))
