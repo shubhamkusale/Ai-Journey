9be19fab28f48eea9d9a544cd2597277ca25f87a
Shubham kusale <98016063+shubhamkusale@users.noreply.github.com>
2026-07-03 19:15:54 +0530

    learned neural network how it works and basics

---
+++ WEEK10/cnn_classifier.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
---
+++ WEEK7/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_siz
