653c087c5ee75c079b177e249b6ca35e64ad02ad
Shubham kusale <98016063+shubhamkusale@users.noreply.github.com>
2026-07-21 22:10:35 +0530

    WEEK11 libraries imported - complete

---
+++ WEEK11/fashion_mnist_improved.py
@@ -0,0 +1,7 @@
+import torch 
+import torch.nn as nn
+import torch.optim as optim 
+from torchvision import datasets, transforms
+from torch.utils.data import dataloader
+import matplotlib.pyplot as plt
+
