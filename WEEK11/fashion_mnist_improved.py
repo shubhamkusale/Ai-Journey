b29a2c84ce379c4ba83802c601d3208304020e18
Shubham kusale <98016063+shubhamkusale@users.noreply.github.com>
2026-07-22 22:19:42 +0530

    WEEK11 transformers setup complete for the process of images  - complete

--- WEEK11/fashion_mnist_improved.py
+++ WEEK11/fashion_mnist_improved.py
@@ -5,3 +5,10 @@ from torchvision import datasets, transforms
 from torch.utils.data import dataloader
 import matplotlib.pyplot as plt
 
+train_transform = transforms.Compose([
+    transforms.RandomRotation(10),
+    transforms.RandomAffine(0, translate=(0.1, 0.1)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5),(0.5))
+    ])
+
