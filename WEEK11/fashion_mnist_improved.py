4fc66895d683b8a74391d872038acfab94f58e00
Shubham kusale <98016063+shubhamkusale@users.noreply.github.com>
2026-07-24 18:01:30 +0530

    WEEK11 data imported from MNIST's dataset - complete

--- WEEK11/fashion_mnist_improved.py
+++ WEEK11/fashion_mnist_improved.py
@@ -12,3 +12,11 @@ train_transform = transforms.Compose([
     transforms.Normalize((0.5),(0.5))
     ])
 
+test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.normalize((0.5),(0.5))]
+)
+
+train_data = datasets.FashionMNIST(root='.data', train=True, download=True,transform=train_transform)
+test_data = datasets.FashionMNIST(root='.data', train=False, download=True,transform=train_transform)
+
