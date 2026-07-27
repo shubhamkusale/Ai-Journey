dd72373d89e282ccb57df6b648931a30821910f2
Shubham kusale <98016063+shubhamkusale@users.noreply.github.com>
2026-07-27 18:30:26 +0530

    WEEK11 Created forward function for connecting the layers of images which will get compressed 3 layers that will connect in forward function - complete

--- WEEK11/fashion_mnist_improved.py
+++ WEEK11/fashion_mnist_improved.py
@@ -48,3 +48,12 @@ class BatchNormCNN(nn.Module):
         self.relu3 = nn.relu()
         self.dropout2 = nn.Dropout(0.5)
         self.fc2 = nn.Linear(128, 10)   
+
+    def forward(self, x):
+        x = self.dropout1(self.pool1(self.relu1(self.bn1(self.conv1(x)))))
+        x = self.dropout2(self.pool2(self.relu2(self.bn2(self.conv2(x)))))
+        x = x.view(-1, 32 * 7 * 7)
+        x = self.dropout3(self.relu3(self.fc1(x)))
+        x = self.fc2(x)
+        return x
+
