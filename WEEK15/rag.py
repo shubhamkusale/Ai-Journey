b80cccf6ed60577f7cad9dcb1d239e357e6e043e
Shubham kusale <98016063+shubhamkusale@users.noreply.github.com>
2026-09-07 18:00:33 +0530

    Week 15 -  small model got in code given aur data to the model

--- WEEK15/rag.py
+++ WEEK15/rag.py
@@ -1,2 +1,10 @@
-from sentence_transformers import sentence_transformer
+from sentence_transformers import SentenceTransformer
+
+embedder = SentenceTransformer('all-MiniLM-L6-v2')
+
+documents = [
+    "Jarvis uses Whisper for speech-to-text conversion.",
+    "Jarvis's memory layer is built using ChromaDB.",
+    "The holographic interface uses Three.js for 3D rendering."
+]
 
