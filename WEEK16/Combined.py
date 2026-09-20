9b6eacea63bd14dc21b63d48ec0aafe9b6d66cd7
Shubham kusale <98016063+shubhamkusale@users.noreply.github.com>
2026-09-20 18:52:57 +0530

    Week 16: Full voice pipeline - STT transcribes speech, LLM generates response, TTS speaks it back

--- WEEK16/Combined.py
+++ WEEK16/Combined.py
@@ -23,3 +23,19 @@ chat_response = client.chat.completions.create(
 )
 answer_text = chat_response.choices[0].message.content
 print("jarvis says:", answer_text)
+
+speech_response = client.audio.speech.create(
+    model="canopylabs/orpheus-v1-english",
+    voice="troy",
+    input=answer_text,
+    response_format="wav"
+)
+
+audio_data = speech_response.read()
+
+p = pyaudio.PyAudio()
+stream = p.open(format=p.get_format_from_width(2),channels=1, rate=24000, output= True)
+stream.write(audio_data[44:])
+stream.stop_stream()
+stream.close()
+p.terminate()
